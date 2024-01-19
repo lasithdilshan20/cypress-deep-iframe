070b5f563c2ff6c8cbbcbed1e1881ba7a9585c94
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2024-01-19 16:35:32 +0800

    Enhance iFrame handling and update Cypress config
    Improved the iFrame interaction code in Cypress E2E tests enabling dynamic traversal and interaction within iFrame elements. "traverseToIframeWithinElements" function was added to support this improvement. Cypress config was also updated by disabling Chrome web security and auto watching for file changes.

--- cypress/e2e/deep-iframe.cy.js
+++ cypress/e2e/deep-iframe.cy.js
@@ -2,12 +2,14 @@ describe('Nested Iframe Interaction', () => {
     it('should interact with elements within nested iframes', () => {
         cy.visit('cypress/html/index.html');
 
+        // Traverse to an iframe within a list of elements
         cy.collectIframeSelectors().then((iframeSelectors) => {
             cy.traverseToIframeWithinElements(iframeSelectors, 'input#inputFieldL1', () => {
                 cy.get('input#inputFieldL1').type("Hello iFrame 1");
             });
         });
 
+        // Traverse to Deepest an iframe within a list of elements
         cy.collectIframeSelectors().then((iframeSelectors) => {
             cy.traverseDeepIframe(iframeSelectors, () => {
                 cy.get('input#inputFieldL3').type("Hello iFrame 3");
