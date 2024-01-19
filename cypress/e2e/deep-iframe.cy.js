67fb64c85c0b3246e005a7e1b79b78ef99529bcc
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2024-01-19 16:33:13 +0800

    Enhance iFrame handling and update Cypress config
    Improved the iFrame interaction code in Cypress E2E tests enabling dynamic traversal and interaction within iFrame elements. "traverseToIframeWithinElements" function was added to support this improvement. Cypress config was also updated by disabling Chrome web security and auto watching for file changes.

--- cypress/e2e/deep-iframe.cy.js
+++ cypress/e2e/deep-iframe.cy.js
@@ -3,8 +3,14 @@ describe('Nested Iframe Interaction', () => {
         cy.visit('cypress/html/index.html');
 
         cy.collectIframeSelectors().then((iframeSelectors) => {
+            cy.traverseToIframeWithinElements(iframeSelectors, 'input#inputFieldL1', () => {
+                cy.get('input#inputFieldL1').type("Hello iFrame 1");
+            });
+        });
+
+        cy.collectIframeSelectors().then((iframeSelectors) => {
             cy.traverseDeepIframe(iframeSelectors, () => {
-                cy.get('input#inputFieldL3').type("Hello iFrame 3")
+                cy.get('input#inputFieldL3').type("Hello iFrame 3");
             });
         });
     });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -61,21 +61,63 @@ Cypress.Commands.add('traverseToIframeContainingElement', (iframeSelectors, targ
         const selector = iframeSelectors[index];
         cy.log(`Current iframe selector: ${selector}`); // Log the current selector
 
-        cy.get(`${selector} ${targetSelector}`).then(($target) => {
-            if ($target.length > 0) {
-                // Found the target element, perform the action.
-                actionCallback();
-            } else {
-                // Element not found inside the current iframe, move on to the next.
-                cy.get(selector).its('0.contentDocument.body').should('not.be.undefined') // Wait for iframe to load
-                    .then((iframeBody) => {
-                        // Switch context to iframe, and recursively call traverseIframes function for next level
-                        cy.wrap(iframeBody).within(() => traverseIframes(index + 1));
-                    });
-            }
-        });
+        cy.get(selector).its('0.contentDocument.body').should('not.be.undefined').then(iFrameBody=> {
+            cy.log(`Current iframe element: ${iFrameBody}`);
+            cy.wrap(iFrameBody).within(() => {
+                cy.log(`Current iframe Wrapped Body: ${cy.wrap(iFrameBody).toString()}`);
+                cy.log(`Current iframe Wrapped Body: ${cy.wrap(iFrameBody).find(targetSelector).toString()}`);
+                if (cy.wrap(iFrameBody).find(targetSelector).length > 0) {
+                    // Found the target element, perform the action.
+                    cy.log(`Found target element ${targetSelector} inside iframe ${selector}`)
+                    actionCallback();
+                } else {
+                    // Element not found inside the current iframe, move on to the next.
+                    cy.get(selector).its('0.contentDocument.body').should('not.be.undefined') // Wait for iframe to load
+                        .then((iframeBody) => {
+                            // Switch context to iframe, and recursively call traverseIframes function for next level
+                            cy.wrap(iframeBody).within(() => traverseIframes(index + 1));
+                        });
+                    cy.log(`Did not find target element ${targetSelector} inside iframe ${selector}`)
+                }
+            });
+        })
     };
 
     // Start traversal from the 1st iframe
     traverseIframes(0);
+});
+
+Cypress.Commands.add("traverseToIframeWithinElements", (iframeSelectors, targetSelector, actionCallback) => {
+    let traverseIframes = (index = 0) => {
+        if (index >= iframeSelectors.length) {
+            return cy.wrap(false);
+        }
+        const selector = iframeSelectors[index];
+        return cy.get(selector).then(($selector) => {
+            if($selector.length) {
+                return cy.window({ log: true }).its('document').then((document) => {
+                    const iFrameBody = document.querySelector(selector).contentDocument.body;
+                    if(iFrameBody) {
+                        return cy.wrap(iFrameBody).within(() => {
+                            cy.get(targetSelector).then(($el) => {
+                                if($el.length) {
+                                    actionCallback();
+                                    return cy.wrap(true);
+                                } else {
+                                    return traverseIframes(index + 1);
+                                }
+                            });
+                        });
+                    } else {
+                        cy.log(`Iframe body is not available for iframe with selector ${selector}`);
+                        return traverseIframes(index + 1);
+                    }
+                });
+            } else {
+                cy.log(`Did not find iframe with selector ${selector}`);
+                return traverseIframes(index + 1);
+            }
+        });
+    };
+    return traverseIframes(0);
 });
